14b96b4ab668127772815c0bdfceb1c7b6ccd971
xzqt23 <angelo.agualada@gmail.com>
2023-02-16 17:56:29 +0800

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# Tuples are ordered,unchangeable and it allows duplicate values
+# It is identical to list but the difference is list are changeable while 
+# tuples are not
+
+
+cars = ("Honda","Hyundai","Ford")
+
+
+print(cars)
